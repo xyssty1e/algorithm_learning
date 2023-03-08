a820bb80b38dbf816f8be89ffbf8f4abedccc491
xyssty1e <49758905+xyssty1e@users.noreply.github.com>
2023-03-09 00:20:49 +0800

    Update shellsort.py

--- ABC/shellsort.py
+++ ABC/shellsort.py
@@ -1,17 +1,20 @@
+# -*- encoding:utf-8 -*-
 # shell sort 
 
 class Solution:
-    def shell_sort(lst):
+    def shell_sort(self, lst):
         step = len(lst) // 2
         while step > 0:
             for i in range(step, len(lst)):
                 j = i
                 tmp = lst[j]
                 while j >= step and lst[j-step] > tmp:
-                    lst[j] = lst[j-stem]
+                    lst[j] = lst[j-step]
                     j -= step
                 lst[j] = tmp
             step = step // 2
             
-
-            s = Solution()
+lst = [5,1,8,4,3,0,9,2,7,6]
+s = Solution()
+s.shell_sort(lst)
+print(lst)
